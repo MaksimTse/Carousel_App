c1eaa96b140977ab06a1a87d73af218bdbe3cf9a
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2024-03-11 22:10:38 +0200

    Add files via upload

--- Carousel_App/MainPage.xaml.cs
+++ Carousel_App/MainPage.xaml.cs
@@ -1,89 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Carousel_App
 {
     public partial class MainPage : CarouselPage
     {
+        string[] imageTitles = {
+            "GIGABYTE GeForce RTX­­™ 4060",
+            "GIGABYTE GeForce RTX­­™ 4070",
+            "GIGABYTE GeForce RTX­­™ 4080",
+            "GIGABYTE GeForce RTX­­™ 4090"
+        };
+
+        string[] imageDescriptions = {
+            "The GIGABYTE GeForce RTX­­™ 4060 GAMING OC 8G is a high-performance graphics card designed for gaming. It features the NVIDIA GeForce RTX 4060 chipset with 8GB of GDDR6 graphics memory. With a 128-bit memory interface and PCIE 4.0 16x support, it delivers smooth and immersive gaming experiences. It also comes with dual slot fansink cooling, 2 HDMI connections, and 2 DisplayPort connections.",
+            "GeForce RTX­­™ 4070 GAMING OC 12G - Powered by NVIDIA DLSS 3, ultra-efficient Ada Lovelace arch, and full ray tracing - 4th Generation Tensor Cores: Up to 4x performance with DLSS 3 vs. brute-force rendering - 3rd Generation RT Cores: Up to 2X ray tracing performance - Powered by GeForce RTX™ 4070 - Integrated with 12GB GDDR6X 192bit memory interface - WINDFORCE cooling system - RGB Fusion - Dual BIOS - Protection metal back plate",
+            "The Gigabyte GeForce RTX 4080 16GB EAGLE OC DLSS 3 is a high-end graphics card designed for gaming and professional use. With 16GB of GDDR6X VRAM and a GPU clock speed of 2520 MHz, this card delivers exceptional performance for demanding applications. It also supports DLSS 3, which uses AI to improve gaming performance and image quality. The card has 3 DisplayPort and 1 HDMI connections, and requires a 900W power supply.",
+            "e Gigabyte GeForce RTX 4090 GAMING OC 24G DLSS 3 is a high-end graphics card designed for gaming and professional use. It features the latest Lovelace architecture from NVIDIA, with new streaming multiprocessors, tensor cores, and RT cores for improved performance and ray tracing capabilities. It also supports DLSS 3 for improved AI-accelerated graphics, and features a dual slot fansink cooling system for efficient heat dissipation."
+        };
+
         public MainPage()
         {
-            var redContentPage = new ContentPage
+            Image[] images = {
+                new Image { Source = "six.png", WidthRequest = 400, HeightRequest = 400, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand },
+                new Image { Source = "seven.png", WidthRequest = 400, HeightRequest = 400, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand },
+                new Image { Source = "eigth.png", WidthRequest = 400, HeightRequest = 400, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand },
+                new Image { Source = "nine.png", WidthRequest = 400, HeightRequest = 400, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand }
+            };
+
+            for (int i = 0; i < images.Length; i++)
             {
-                Content = new StackLayout
+                int index = i;
+
+                images[i].GestureRecognizers.Add(new TapGestureRecognizer
                 {
-                    Children =
+                    Command = new Command(() => DisplayAlert(imageTitles[index], imageDescriptions[index], "Osta"))
+                });
+
+                Children.Add(new ContentPage
+                {
+                    Content = new StackLayout
                     {
-                        new Label
-                        {   
-                            Text = "Red",
-                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions  = LayoutOptions.Center
-                        },
-                        new BoxView
+                        Children =
                         {
-                            Color = Color.Red,
-                            WidthRequest = 200,
-                            HeightRequest = 200,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand
+                            new Label
+                            {
+                                Text = imageTitles[index],
+                                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                                HorizontalOptions = LayoutOptions.Center
+                            },
+                            images[index]
                         }
                     }
-                }
-            };
-            var greenContentPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    Children =
-                    {
-                        new Label
-                        {
-                            Text = "Green",
-                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions  = LayoutOptions.Center
-                        },
-                        new BoxView
-                        {
-                            Color = Color.Green,
-                            WidthRequest = 200,
-                            HeightRequest = 200,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand
-                        }
-                    }
-                }
-            };
-            var blueContentPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    Children =
-                    {
-                        new Label
-                        {
-                            Text = "Blue",
-                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions  = LayoutOptions.Center
-                        },
-                        new BoxView
-                        {
-                            Color = Color.Blue,
-                            WidthRequest = 200,
-                            HeightRequest = 200,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand
-                        }
-                    }
-                }
-            };
-            Children.Add(redContentPage);
-            Children.Add(greenContentPage);
-            Children.Add(blueContentPage);
+                });
+            }
         }
     }
 }
